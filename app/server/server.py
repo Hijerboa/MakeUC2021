5c8d72ffe31c752ada61e35a5a2554c9bcadcd82
Noxid00 <Noxid00@gmail.com>
2021-10-09 15:45:49 -0400

    server base + requirements.txt update

--- app/server/server.py
+++ app/server/server.py
@@ -8,20 +8,30 @@ import plotly.express as px
 import pandas as pd
 import sqlalchemy as db
 from sqlalchemy.sql import func
+import dash_daq as daq
+import dash_bootstrap_components as dbc
 
+def get_fig():
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/earthquakes-23k.csv') # Placeholder
 
-def get_app():
-    app = dash.Dash(__name__)
-
-    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/earthquakes-23k.csv')
-
-    px.set_mapbox_access_token(get_secret("mapbox_api_key"))
     fig = px.density_mapbox(df, lat='Latitude', lon='Longitude', z='Magnitude', radius=10,
                             center=dict(lat=0, lon=180), zoom=1,
                             mapbox_style="dark")
+    
+    fig.update_layout(
+        margin=dict(l=0,r=0,b=0,t=0),
+        paper_bgcolor="Black"
+    )
+
+    return fig
+
+def get_app():
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+    px.set_mapbox_access_token(get_secret("mapbox_api_key"))
 
     app.layout = html.Div(children=[
-        dcc.Graph(id='map-graph', figure=fig)
+        daq.Slider(min=0, max=100, value=20),
+        dcc.Graph(id='map-graph', figure=get_fig())
     ])
 
     return app
