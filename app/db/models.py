c0d3b58abd72d42b89744b3788fac67757fab89b
Nick Cleary <nick@nickcleary.tech>
2021-10-09 14:01:52 -0400

    Data models go brrrr

--- app/db/models.py
+++ app/db/models.py
@@ -9,3 +9,10 @@ Base = declarative_base()
 
 class PrimaryKeyBase:
     id = Column(Integer, primary_key=True, autoincrement=True)
+
+class TerroristAct(Base):
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer, nullable=False)
+    month = Column(Integer(), nullable=False)
+    day = Column(Integer(), nullable=False)
+    approx_date = Column(String(length=64), nullable=True)
