d3da2f1ae3e935a5b6d9ba1c2a31a9b1f94b7297
Nick Cleary <nick@nickcleary.tech>
2021-10-09 22:46:10 -0400

    wtf

--- app/scripts/add_terror_casualty.py
+++ app/scripts/add_terror_casualty.py
@@ -5,30 +5,35 @@ from datetime import date
 from db.db_utils import get_single_object, create_single_object
 
 def search_list(list, search_id):
+    print('entered search')
     for item in list:
         if item.id == search_id:
+            print('returning')
             return item
     return None
 
 def gen_terrorists():
     initialize()
     session = create_session()
-    all_objects_query = session.query(TerroristAct)
+    """all_objects_query = session.query(TerroristAct).limit(1000).offset(1000)
     all_objects = []
     for object in all_objects_query:
-        all_objects.append(object)
+        all_objects.append(object)"""
     print('Objects added')
     with open('scripts/data.csv', encoding = "ISO-8859-1") as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
         line_count = 0
         objects = []
         for row in csv_reader:
-            if line_count == 0:
+            if line_count == 0 or line_count <= 109000:
                 line_count += 1
+                continue
             else:
-                db_object: TerroristAct = search_list(all_objects, int(row['eventid']))
+                db_object: TerroristAct = get_single_object(session, TerroristAct, id=int(row['eventid']))
                 if db_object is None:
+                    print("None found :(")
                     continue
+                print(db_object.id)
                 if row['doubtterr'] == 1:
                     db_object.doubout_terrorism = True
                 elif row['doubtterr'] == 0:
@@ -46,7 +51,9 @@ def gen_terrorists():
                 if not row['nkillter'] == '':
                     db_object.num_perp_killed = int(row['nkillter'])
                 if not row['nwound'] == '':
-                    db_object.num_injured = int(row['nwound'])
+                    string = row['nwound']
+                    string_split = string.split('.')
+                    db_object.num_injured = int(string_split[0])
                 if not row['nwoundus'] == '':
                     db_object.num_injured_us = int(row['nwoundus'])
                 if not row['nwoundte'] == '':
@@ -54,7 +61,6 @@ def gen_terrorists():
 
                 objects.append(db_object)
                 line_count += 1
-                print(db_object.id)
 
             if len(objects) >= 500:
                 session.bulk_save_objects(objects)
