a8e80945b29ccc9dc8907614e035a132dc680789
Noxid00 <Noxid00@gmail.com>
2021-10-09 21:26:09 -0400

    Changed map to Scatter, added info box, other metrics

--- app/server/server.py
+++ app/server/server.py
@@ -19,17 +19,25 @@ def get_fig():
     session = conn.create_session()
 
     start = time.time()
-    res = session.query(TerroristAct).all()
+    res = session.query(TerroristAct).limit(10000).all()
+    t_query = time.time()
+    print(f'Query took {t_query - start} seconds.')
+    
 
     dates = [r.date for r in res]
     lats = [r.latitude for r in res]
     lngs = [r.longitude for r in res]
     summaries = [r.summary for r in res]
-    cas_counts = [1 for _ in res]
+    killed_counts = [r.num_killed for r in res]
+    injured_counts = [r.num_injured for r in res]
+
+    cas_counts = [(k if not k == None else 0) + (w if not w == None else 0) for k, w in zip(killed_counts, injured_counts)]
+    log_cas_counts = [math.log(c) + 1 if c > 0 else 1 for c in cas_counts]
+    has_casualties = [True if c > 0 else False for c in cas_counts]
 
     #random circular offsets:
     us = [random.random() + random.random() for _ in res]
-    rs = [0.1 * (2 - u if u > 1 else u) for u in us]
+    rs = [0.01 * (2 - u if u > 1 else u) for u in us]
     thetas = [random.random() * 2 * math.pi for _ in res]
     x_off = [r * math.cos(theta) for r, theta in zip(rs, thetas)]
     y_off = [r * math.sin(theta) for r, theta in zip(rs, thetas)]
@@ -47,21 +55,28 @@ def get_fig():
         'Latitude': lats,
         'Longitude': lngs,
         'Casualties': cas_counts,
-        'Summary': summaries
+        'Casualties (x 10^n)' : log_cas_counts,
+        'Killed': killed_counts,
+        'Injured': injured_counts,
+        'Summary': summaries,
+        'Has Casualties': has_casualties
     })
 
     print('data loaded')
     #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/earthquakes-23k.csv') # Placeholder
 
-    fig = px.density_mapbox(df, lat='Latitude', lon='Longitude', z='Casualties', radius=3,
+    fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude',
                             center=dict(lat=0, lon=180), zoom=1,
                             mapbox_style="dark", 
+                            color='Has Casualties',
                             hover_data={
                                 'Latitude': False,
                                 'Longitude': False,
                                 'Date': True,
                                 'Casualties': True,
-                                'Summary': True
+                                'Killed': False,
+                                'Injured': False,
+                                'Summary': False
                             })
 
     print('data plotted')
@@ -97,7 +112,8 @@ def get_app():
     index_layout = html.Div(children=[
         html.H1(id='index-header', children='Main Page', ),
         daq.Slider(id='date-slider', min=0, max=100, value=20),
-        dcc.Graph(id='map-graph', figure=get_fig())
+        dcc.Graph(id='map-graph', figure=get_fig()),
+        html.Div(id='summary-container', children='')
     ])
 
     about_layout = html.Div(children=[
@@ -116,6 +132,36 @@ def get_app():
         else:
             return index_layout
     # You could also return a 404 "URL not found" page here
+
+    @app.callback(
+        dash.dependencies.Output('summary-container', 'children'),
+        dash.dependencies.Input('map-graph', 'hoverData'))
+    def display_hover_data(hoverData):
+        print(hoverData)
+        summary = hoverData['points'][0]['customdata'][6]
+        injured = hoverData['points'][0]['customdata'][5]
+        killed = hoverData['points'][0]['customdata'][4]
+        
+        if injured == None:
+            injured = 'None Reported'
+        elif injured == '0':
+            injured = 'No injuries.'
+
+        if killed == None:
+            killed = 'None Reported'
+        elif killed == '0':
+            killed = 'No deaths.'
+
+        if summary == '' or summary == None:
+            summary = 'No summary available.'
+
+        #print(summary)
+
+        kill_elem = html.P(id='kill-count-text', children=f'Deaths: {killed}')
+        injured_elem = html.P(id='injured-count-text', children=f'Injuries: {injured}')
+        summary_elem = html.P(id='summary-text', children=summary)
+
+        return [kill_elem, injured_elem, summary_elem]
 
     return app
 
